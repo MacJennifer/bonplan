0aef47dfe5fcd0485090909fbd5598aefcc50364
Jennifer Macaire <macaire.jennifer@sfr.fr>
2024-02-09 16:12:28 +0100

    mise à jour base de données crud ok

--- backend/database/migrations/2024_02_06_145117_create_comments_table.php
+++ backend/database/migrations/2024_02_06_145117_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('comment');
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
